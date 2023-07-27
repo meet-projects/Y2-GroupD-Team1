4936fdec2039d369f920d639b9beda5cc6a7b895
Yasminta24-meet <109975287+YasminQT@users.noreply.github.com>
2023-07-27 13:38:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
-
+config = {
+  'apiKey': "AIzaSyCDeIwgP6BcZLCnqIOA-41jRm4U6SkCPBo",
+  'authDomain': "esha-le-esha.firebaseapp.com",
+  'projectId': "esha-le-esha",
+  'storageBucket': "esha-le-esha.appspot.com",
+  'messagingSenderId': "537908405410",
+  'appId': "1:537908405410:web:e6cda3c540ba6fd62b23fb",
+  'measurementId': "G-DK1D72K916",
+  "databaseURL":'https://esha-le-esha-default-rtdb.europe-west1.firebasedatabase.app/'
+};
 
 
 
